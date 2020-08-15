76ce875aaf28f304deb153a3f414e5a52ab33062
Verta-master <vertyagina@gmail.com>
2020-08-15 17:17:12 +0600

    Selection

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //Menu
 $('.menu__open').click(function() {
-  $(this).hide();
-  $('.menu__close').show();
+  $(this).fadeOut();
+  $('.menu__close').fadeIn();
   $('.menu__body').slideDown();
 });
 
 $('.menu__close').click(function() {
-  $(this).hide();
-  $('.menu__open').show();
+  $(this).fadeOut();
+  $('.menu__open').fadeIn();
   $('.menu__body').slideUp();
 });
