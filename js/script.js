db9ea28eba218d823c71a07bc9f34a8aa882c54f
Verta-master <vertyagina@gmail.com>
2020-08-19 16:51:18 +0600

    Update 2020-08-19T10:51:13.539Z

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 //Menu
 $('.menu__open').click(function() {
-  $(this).fadeOut();
-  $('.menu__close').fadeIn();
+  $(this).addClass('menu__open--hide');
+  $('.menu__close').addClass('menu__close--visible');
   $('.menu__body').slideDown();
   $('.menu').addClass('menu--open');
 });
 
 $('.menu__close').click(function() {
-  $(this).fadeOut();
-  $('.menu__open').fadeIn();
+  $(this).removeClass('menu__close--visible');
+  $('.menu__open').removeClass('menu__open--hide');
   $('.menu__body').slideUp();
   $('.menu').removeClass('menu--open');
 });
